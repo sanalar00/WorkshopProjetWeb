e648095a7a02a65a3a72edc05251ac115212ada0
sanalar00 <126274585+sanalar00@users.noreply.github.com>
2024-02-27 09:41:18 +0100

    Update showEmployee.php

--- workshop5/Views/showEmployee.php
+++ workshop5/Views/showEmployee.php
@@ -1,15 +1,15 @@
 <?php 
 include '../Model/employee.php';
 
-$client = new Client();
-$client->lastName = 'Doe';
-$client->firstName = 'Jane';
-$client->phone = '123456789';
-$client->address = '123, Liberty Avenue, Tunis';
-$client->dOB = '1/1/2000';
+$employee = new employee();
+$employee->lastName = 'Doe';
+$employee->firstName = 'Jane';
+$employee->phone = '123456789';
+$employee->address = '123, Liberty Avenue, Tunis';
+$employee->dOB = '1/1/2000';
 
 var_dump($employee);
 echo '<br>';
 echo '<hr>';
 echo '<br>';
-$client->show();
+$employee->show();
